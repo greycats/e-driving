248236620fba91a3216115b4b56f33f9dec41537
Tony Jin <tony.jin@interactivelabs.co>
2016-02-26 11:24:06 +0800

    add "MilesView"

---
+++ e-driving/MilesView.swift
@@ -0,0 +1,50 @@
+//
+//  MilesView.swift
+//  e-driving
+//
+//  Created by Jint on 2/26/16.
+//  Copyright © 2016 Interactive Labs. All rights reserved.
+//
+
+import Greycats
+
+@IBDesignable
+class MilesView: NibView {
+    @IBOutlet weak var mileLabel: UILabel! {
+        didSet { renderMile() }
+    }
+    @IBInspectable var mile: String? {
+        didSet { renderMile() }
+    }
+    private func renderMile() {
+        mileLabel?.text = mile
+    }
+    
+    @IBOutlet weak var mileView: UIView! {
+        didSet { renderColor() }
+    }
+    @IBInspectable var mileColor: UIColor? {
+        didSet { renderColor() }
+    }
+    private func renderColor() {
+        mileView?.backgroundColor = mileColor
+    }
+    
+    @IBOutlet weak var positionLabel: PositionLabel! {
+        didSet { renderPosition() }
+    }
+    @IBInspectable var title: String? {
+        didSet { renderPosition() }
+    }
+    @IBInspectable var address: String? {
+        didSet { renderPosition() }
+    }
+    @IBInspectable var circleColor: UIColor? {
+        didSet { renderPosition() }
+    }
+    private func renderPosition() {
+        positionLabel?.titleLabel?.text = title?.uppercaseString
+        positionLabel?.addressLabel?.text = address
+        positionLabel?.circleView?.borderColor = circleColor
+    }
+}
