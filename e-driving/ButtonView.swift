74804fa0c66c3b100f7a5628d400208a8ae11d59
Tony Jin <tony.jin@interactivelabs.co>
2016-02-26 08:55:22 +0800

    update xib

---
+++ e-driving/ButtonView.swift
@@ -0,0 +1,22 @@
+//
+//  Button.swift
+//  e-driving
+//
+//  Created by Jint on 2/26/16.
+//  Copyright © 2016 Interactive Labs. All rights reserved.
+//
+
+import Greycats
+
+@IBDesignable
+class ButtonView: NibView {
+    @IBOutlet weak var buttonView: UIButton! {
+        didSet { renderTitle() }
+    }
+    @IBInspectable var title: String? {
+        didSet { renderTitle() }
+    }
+    private func renderTitle() {
+        buttonView?.setTitle(title, forState: UIControlState.Normal)
+    }
+}
