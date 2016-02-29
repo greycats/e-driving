5dff19313d37fc2dd0e9a7ccb754046dfb63a717
Rex Sheng <shengning@gmail.com>
2016-02-29 18:57:34 +0800

    implement shadow

--- e-driving/VehicleScene.swift
+++ e-driving/VehicleScene.swift
@@ -20,9 +20,10 @@ class VehicleViewController: UIViewController, ColorPalette {
 	@IBOutlet weak var hardAcceleLabel: IndexLabel!
 	@IBOutlet weak var overSpeedLabel: IndexLabel!
 	@IBOutlet weak var experienceView: ExperienceView!
+	@IBOutlet weak var separator: UIView!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
 		applyTheme(.Dark)
 		for subview in scrollView.subviews {
 			if let subview = subview as? ColorPalette {
@@ -34,6 +35,12 @@ class VehicleViewController: UIViewController, ColorPalette {
 				subview.applyTheme(.Dark)
 			}
 		}
+
+		separator.layer.shadowColor = UIColor(white: 0, alpha: 0.3).CGColor
+		separator.layer.shadowRadius = 10
+		separator.layer.shadowOpacity = 1
+		separator.layer.shadowPath = UIBezierPath(rect: CGRect(x: -100, y: 0, width: ScreenSize.width + 200, height: 10)).CGPath
+
 		routeView.displayMiles = false
 		routeView.route = [
 			RouteHistory(timestamp: NSDate(), location: CLLocation(latitude: 37.662438, longitude: -122.424233), recoginzedName: "From Home", milesToNext: 6),
