2aae0a0536899d7ad8b938173fed64603e7d5e4d
Rex Sheng <shengning@gmail.com>
2016-03-01 16:56:02 +0800

    enhance clock view

--- e-driving/MilesView.swift
+++ e-driving/MilesView.swift
@@ -12,7 +12,18 @@ import Greycats
 class ClockView: UIView, ColorPalette {
 	@IBInspectable var onColor: UIColor = UIColor.blackColor() { didSet { setNeedsDisplay() } }
 	@IBInspectable var offColor: UIColor = UIColor(hexRGB: 0x163782) { didSet { setNeedsDisplay() } }
-	@IBInspectable var percentage: Double = 0 { didSet { setNeedsDisplay() } }
+	var offAfter: Int = 0
+	let needleCount = 36
+
+	@IBInspectable var percentage: Double = 0 {
+		didSet {
+			let int = Int((floor(min(1, percentage) * Double(needleCount))))
+			if int != offAfter {
+				offAfter = int
+				setNeedsDisplay()
+			}
+		}
+	}
 
 	func setColor(color: UIColor, category: ColorCategory) {
 		switch category {
@@ -22,26 +33,42 @@ class ClockView: UIView, ColorPalette {
 			break
 		}
 	}
-	@IBInspectable var needleSize: CGSize = CGSize(width: 1.15, height: 5.57) { didSet { setNeedsDisplay() } }
+	var paths: [UIBezierPath] = []
+	var origin: CGPoint!
+
+	@IBInspectable var needleSize: CGSize = CGSize(width: 1.15, height: 5.57) {
+		didSet {
+			let circleSize = bounds.size
+			let startPoint = CGPoint(x: -needleSize.width / 2, y: -circleSize.height / 2)
+			origin = CGPoint(x: circleSize.width / 2, y: -startPoint.y)
+			let cornerRadius = needleSize.width / 2
+			paths = (0..<needleCount).map { _ in
+				UIBezierPath(roundedRect: CGRect(origin: startPoint, size: needleSize), cornerRadius: cornerRadius)
+			}
+		}
+	}
+
 	override func drawRect(rect: CGRect) {
-		let angle = CGFloat(2 * M_PI / 45)
-		let offAfter = Int(floor(percentage * 45))
-		let circleSize = rect.size
-		let startPoint = CGPoint(x: -needleSize.width / 2, y: -circleSize.height / 2)
-		let cornerRadius = needleSize.width / 2
 		let context = UIGraphicsGetCurrentContext()
 		CGContextSaveGState(context)
-		CGContextTranslateCTM(context, circleSize.width / 2 + startPoint.x, -startPoint.y)
-		for i in 0..<45 {
-			let off = i >= offAfter
-			let path = UIBezierPath(roundedRect: CGRect(origin: startPoint, size: needleSize), cornerRadius: cornerRadius)
+		CGContextTranslateCTM(context, origin.x, origin.y)
+		let angle = CGFloat(2 * M_PI / Double(needleCount))
+
+		for i in 0..<offAfter {
+			let path = paths[i]
 			CGContextAddPath(context, path.CGPath)
 			CGContextRotateCTM(context, angle)
-			CGContextSetFillColorWithColor(context, off ? offColor.CGColor : onColor.CGColor)
-			CGContextFillPath(context)
 		}
+		CGContextSetFillColorWithColor(context, onColor.CGColor)
+		CGContextFillPath(context)
+		for i in offAfter..<needleCount {
+			let path = paths[i]
+			CGContextAddPath(context, path.CGPath)
+			CGContextRotateCTM(context, angle)
+		}
+		CGContextSetFillColorWithColor(context, offColor.CGColor)
+		CGContextFillPath(context)
 		CGContextRestoreGState(context)
-		super.drawRect(rect)
 	}
 }
 
