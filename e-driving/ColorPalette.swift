5a7eb654571fbbbd396509a4c00edba21e84d155
Rex Sheng <shengning@gmail.com>
2016-03-01 09:49:27 +0800

    optimize color palette

--- e-driving/ColorPalette.swift
+++ e-driving/ColorPalette.swift
@@ -27,8 +27,33 @@ protocol ColorPalette {
 	func applyTheme(theme: Theme)
 }
 
+private let Light: [ColorCategory: UIColor] = [
+	.Background: UIColor(hexRGB: 0x000000),
+	.Foreground: UIColor(hexRGB: 0xF7F7F7),
+	.MainText: UIColor(hexRGB: 0x1F2C3A),
+	.SupplymentText: UIColor(hexRGB: 0x9B9B9B),
+	.Highlight: UIColor(hexRGB: 0xE67E22)
+]
+
+private let Dark: [ColorCategory: UIColor] = [
+	.Background: UIColor(hexRGB: 0x000E31),
+	.Foreground: UIColor(hexRGB: 0x072E86),
+	.MainText: UIColor(hexRGB: 0xFFFFFF),
+	.SupplymentText: UIColor(hexRGB: 0x8396C0),
+	.Highlight: UIColor(hexRGB: 0x50D5C2)
+]
+
 extension ColorPalette {
 	func setColor(color: UIColor, category: ColorCategory) {
+	}
+
+	private func _applyTheme(theme: Theme) {
+		switch theme {
+		case .Light:
+			Light.forEach { setColor($1, category: $0) }
+		case .Dark:
+			Dark.forEach { setColor($1, category: $0) }
+		}
 	}
 
 	func applyTheme(theme: Theme) {
@@ -38,21 +63,7 @@ extension ColorPalette {
 				field.applyTheme(theme)
 			}
 		}
-
-		switch theme {
-		case .Light:
-			setColor(UIColor(hexRGB: 0x000000), category: .Background)
-			setColor(UIColor(hexRGB: 0xF7F7F7), category: .Foreground)
-			setColor(UIColor(hexRGB: 0x1F2C3A), category: .MainText)
-			setColor(UIColor(hexRGB: 0x9B9B9B), category: .SupplymentText)
-			setColor(UIColor(hexRGB: 0xE67E22), category: .Highlight)
-		case .Dark:
-			setColor(UIColor(hexRGB: 0x000E31), category: .Background)
-			setColor(UIColor(hexRGB: 0x072E86), category: .Foreground)
-			setColor(UIColor(hexRGB: 0xFFFFFF), category: .MainText)
-			setColor(UIColor(hexRGB: 0x8396C0), category: .SupplymentText)
-			setColor(UIColor(hexRGB: 0x50D5C2), category: .Highlight)
-		}
+		_applyTheme(theme)
 	}
 }
 
