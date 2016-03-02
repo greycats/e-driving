a6c60b76f71a1a5d017df814602c99ce10f3bb88
Rex Sheng <shengning@gmail.com>
2016-03-02 16:43:06 +0800

    performance upper space

---
+++ e-driving/AlertInfoView.swift
@@ -0,0 +1,46 @@
+//
+//  AlertInfoView.swift
+//  e-driving
+//
+//  Created by Rex Sheng on 3/2/16.
+//  Copyright © 2016 Interactive Labs. All rights reserved.
+//
+
+import Greycats
+
+
+struct CarAlert {
+	enum Reason {
+		case LowOil
+		case LowGas
+		case EngineCheck
+	}
+	let reason: Reason
+	let error: String
+	let suggestion: String
+}
+
+class AlertInfoView: NibView {
+	@IBOutlet weak var reasonLabel: UILabel!
+	@IBOutlet weak var errorLabel: UILabel!
+	@IBOutlet weak var suggestionLabel: UILabel!
+	@IBOutlet weak var icon: UIImageView!
+
+	convenience init(alert: CarAlert) {
+		self.init(frame: .zero)
+		switch alert.reason {
+		case .LowGas:
+			icon.image = UIImage(named: "low_gas")
+			reasonLabel.text = "Low Gas"
+		case .LowOil:
+			icon.image = UIImage(named: "low_oil")
+			reasonLabel.text = "Low Oil"
+		case .EngineCheck:
+			icon.image = UIImage(named: "engine_check")
+			reasonLabel.text = "Engine Check"
+		}
+		errorLabel.text = alert.error.uppercaseString
+		suggestionLabel.text = alert.suggestion.capitalizedString
+	}
+}
+
--- e-driving/CarPerformanceScene.swift
+++ e-driving/CarPerformanceScene.swift
@@ -6,14 +6,22 @@
 //  Copyright © 2016 Interactive Labs. All rights reserved.
 //
 
-import UIKit
+import Greycats
 
 class CarPerformanceViewController: UIViewController, ColorPalette, Overlayed {
 	@IBOutlet weak var vehicleView: VehicleView!
 	@IBOutlet weak var findMechanic: ButtonView!
 
+	@IBOutlet weak var alertsView: UIView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		applyTheme(.Dark)
+		let alerts = [
+			CarAlert(reason: .LowOil, error: "change in 20 miles", suggestion: "Needs Replacement"),
+			CarAlert(reason: .LowGas, error: "20 miles left", suggestion: "Find Nearest Station"),
+			CarAlert(reason: .EngineCheck, error: "20 miles left", suggestion: "Engine Maintenance")
+		]
+		let views = alerts.map { AlertInfoView(alert: $0) }
+		alertsView |< views
 	}
 }
